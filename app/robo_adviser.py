666270d4b30a125603eff7037488d1d915b61225
carolinefeeney <46456081+carolinefeeney@users.noreply.github.com>
2019-02-24 12:37:35 -0500

    Issue a request

--- app/robo_adviser.py
+++ app/robo_adviser.py
@@ -1,10 +1,36 @@
 from dotenv import load_dotenv
-import json
+import json #included in python language so don't need to install
 import os
 import requests
 from IPython import embed
 
 load_dotenv() # loads environment variables set in a ".env" file, including the value of the ALPHAVANTAGE_API_KEY variable
+
+#
+# INFO INPUTS
+#
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
+
+response = requests.get(request_url)
+# print(type(response)) #> class 'requests.models.reponse'
+# print(response.status_code) #> '200' --> successful
+# print (response.text) #> this text is a string so we need to use JSON to process into a dictionary
+
+parsed_response = json.loads(response.text)  #> use this to parse from a string into a dictionary
+
+breakpoint()
+
+
+
+
+quit()
+
+#
+# INFO OUTPUTS
+#
+
+
 
 # see: https://www.alphavantage.co/support/#api-key
 api_key = os.environ.get("ALPHAVANTAGE_API_KEY") or "OOPS. Please set an environment variable named 'ALPHAVANTAGE_API_KEY'."
